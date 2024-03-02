aff07bf61be9fe8d5183d93b89d254b91e7b0982
unknown <wanyonyidenis532@gmail.com>
2024-03-02 08:39:40 +0300

    first commit

---
+++ trying.dart
@@ -0,0 +1,19 @@
+void main () {
+  //Example of int data type
+  int age = 21;
+  print('Age: = $age');
+
+  //Example of double data type
+  double temperature = 90.1;
+  print('Temperature : $temperature');
+  //Example of String data type
+  String name = 'Denis Matata';
+  print('Name: $name');
+  //Example of list data type
+  List<int> numbers = [1,2,3];
+  print('Numbers: $numbers');
+  //Example of boolean data type
+  bool isSingle =false;
+  print('Is he single? $isSingle');
+
+}
